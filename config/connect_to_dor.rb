4dae5093e3c05b4e20323ab6ee382c31c1fcb214
Joe Atzberger <ohiocore@gmail.com>
2015-02-23 19:43:01 -0800

    Avoid rspec failure with missing config
    Only include it if it is there.
    There are alternative approaches but this is the simplest fix.
    
    Also this variable does not need to be a GLOBAL.

--- config/connect_to_dor.rb
+++ config/connect_to_dor.rb
@@ -2,5 +2,5 @@ environment  = ENV['ENVIRONMENT'] ||= 'development'
 GEM_ROOT = File.expand_path(File.dirname(__FILE__) + '/..')
 
 # Environment.
-ENV_FILE = GEM_ROOT + "/config/environments/#{environment}.rb"
-require ENV_FILE
+env_file = GEM_ROOT + "/config/environments/#{environment}.rb"
+require env_file if File.file?(env_file)
